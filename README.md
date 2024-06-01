b4b6c56f9a39c8016499a6be635aaa1bed6f900f
Aaron Miller <aaronmiller@gmail.com>
2024-06-01 15:23:29 -0700

    seeding

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # remove-exclusions
-Remove all exclusions created by the given excluder
+Remove all exclusions created by the given excluder. BE CAREFUL. Use at your own risk.
+
+## Prerequisites
+
+[Install deno](https://docs.deno.com/runtime/manual/getting_started/installation)
+
+## Usage
+
+1. in `remove.ts` replace the `excluder` parameter w/ the email of the `Excluded By` value from OX for which you wish to remove ALL the Exclusions associated with that id.
+2. in the .env file (create it if you don't have one), add your `OX_API_KEY=<value_here>`
+3. run `deno run -A remove.ts` (the `-A` allows all permissions)
+
+## Expected output
+
+An array of removed exclusionIds, something like this:
+
+```
+[
+  {
+    data: { removeAlertExclusion: { id: "665b98abd40a3e5f2de452e8" } }
+  },
+  {
+    data: { removeAlertExclusion: { id: "665b98abd40a3e5f2de452d7" } }
+  },
+  <snip>
+]
+```
+
+If there are no exclusions created by the given excluder, the output is simply: `[]`
---
+++ remove.ts
@@ -0,0 +1,68 @@
+import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
+
+async function getExclusions (excluder: string, authKey: string): Promise<any> {
+	const myHeaders = new Headers();
+	myHeaders.append("Authorization", authKey);
+	myHeaders.append("Content-Type", "application/json");
+	
+	const graphql = JSON.stringify({
+	  query: "query GetExclusions($getExclusionsInput: GetExclusionsInput) { \
+		 getExclusions(getExclusionsInput: $getExclusionsInput) { \
+			exclusions { \
+			  exclusionId \
+			} \
+			totalFilteredExclusions \
+		 } \
+	  }",
+	  variables: {"getExclusionsInput":{"filters":{"modifiedBy":[excluder]},"offset":0,"limit":50}}
+	});
+	const requestOptions = {
+	  method: "POST",
+	  headers: myHeaders,
+	  body: graphql,
+	  redirect: "follow"
+	};
+	
+	const queryResponse = await fetch("https://api.cloud.ox.security/api/apollo-gateway", requestOptions);
+	const parsedResponse = await queryResponse.json();
+   return parsedResponse;
+}
+
+async function deleteExclusion (exclusionId: string, authKey: string): Promise<any> {
+   const myHeaders = new Headers();
+	myHeaders.append("Authorization", authKey);
+	myHeaders.append("Content-Type", "application/json");
+	
+	const graphql = JSON.stringify({
+		query: "mutation RemoveAlertExclusion($exclusionId: String!) {  \
+			removeAlertExclusion(exclusionId: $exclusionId) {    \
+					  id  \
+			}\
+		}",
+	  variables: {"exclusionId":exclusionId}
+	})
+	const requestOptions = {
+	  method: "POST",
+	  headers: myHeaders,
+	  body: graphql,
+	  redirect: "follow"
+	};
+	
+	const queryResponse = await fetch("https://api.cloud.ox.security/api/apollo-gateway", requestOptions);
+	const parsedResponse = await queryResponse.json();
+	return parsedResponse;
+}
+
+const env = await load();
+const authKey = env["OX_API_KEY"];
+const excluder = "test@ox.security";
+const parsedResponse = await getExclusions(excluder, authKey);
+const exclusionsArray = parsedResponse.data.getExclusions.exclusions;
+
+const parsedStringArray = new Array();
+for (let i = 0; i < exclusionsArray.length; i++) {
+   const exclusionResult = await deleteExclusion(exclusionsArray[i].exclusionId, authKey);
+   const resultString = await JSON.stringify(exclusionResult);
+   parsedStringArray.push(await JSON.parse(resultString)); //.parse undoes overescaping caused by .stringify
+}
+console.log(parsedStringArray);
