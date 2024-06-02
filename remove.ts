e54d3490ffd976d724e27ae3edf1e05d68a9da7e
Aaron Miller <aaronmiller@gmail.com>
2024-06-02 09:54:41 -0700

    wrestling w/ array declarations

--- remove.ts
+++ remove.ts
@@ -87,7 +87,7 @@ interface Exclusion {
 const env = await load();
 const authKey = env["OX_API_KEY"];
 const excluder = "test@ox.security";
-let exclusionsArray = new Array(Exclusion);
+let exclusionsArray: Exclusion[] = new Array(Exclusion);
 const parsedResponse: GetExclusionsResponse = await getExclusions(
   excluder,
   authKey,
